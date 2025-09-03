b51ae4dbfb8c026d9c20cba91758c10f032147a4
Zaheer Ali <zaheerali334409@gmail.com>
2025-09-03 14:58:44 +0500

    Object

---
+++ Object/app.js
@@ -0,0 +1,58 @@
+//console.log("Hello World");
+// Null And Undefined
+// let username;
+// console.log(username);
+
+// assign , string "",number 2321,Boolean ture false , Array [],Object {},
+// let num = null
+// console.log(num);
+
+// Object
+// let apple = {
+//     taste: "Sweet",
+//     color: "Red",
+//     MakeBloodCell: true,
+//     Shape: "Spherical",
+// }
+// console.log(apple.taste);
+// console.log(apple.Shape);
+// console.log(apple.color);
+// console.log(apple["Shape"]);  //aise bhi check hoga
+
+// console.log(apple);
+// // change value in object
+// console.log(apple.MakeBloodCell = "false");
+// apple["taste"] = "bitter-sweet";
+// console.log(apple);
+// // add key
+
+// apple.inFresh = false;
+// console.log(apple);
+
+// HOSTING PLANS
+
+// BOOK page Number  220;
+
+
+// function Product(Monthly, Profissinal, UIlimit) {
+//     this.basic = Monthly;
+//     this.Profissinal = Profissinal;
+//     this.UIlimit = UIlimit;
+// }
+// let unit = new Product("40.3", "290.8", "787.2");
+// console.log(unit);
+// document.write(unit);
+
+
+// uncomplated code print in document ;
+
+
+// let p = document.createElement("p");
+// let ptext = document.createTextNode(unit.value);
+// p.appendChild(ptext);
+// document.body.appendChild(p);
+
+
+// Practice________Object.
+let step = startNumber <= endNumber ? 1 : -1;
+console.log(step);
