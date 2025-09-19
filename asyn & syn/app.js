45f681b5bfa7a9cc1c6f1149fc4e43eb4215221d
Zaheer Ali <zaheerali334409@gmail.com>
2025-09-19 10:45:47 +0500

    pomise

--- asyn & syn/app.js
+++ asyn & syn/app.js
@@ -38,21 +38,49 @@
 
 // ---
 
-function signup(email, password, callBack, secondCallBack) {
+// function signup(email, password, callBack, secondCallBack) {
+//     setTimeout(function () {
+//         if (email === "za@gmail.com" && password === "090909") {
+//             console.log("Ok");
+//             callBack(secondCallBack);
+//         }
+//     }, 1000)
+// }
+
+// signup("za@gmail.com", "090909",
+//     function redirectTOTimeLine(cd) {
+//         console.log(cd());
+//     }, function redi() {
+//         return "Wah Bhai Clean Code 😂"
+
+//     });
+
+
+
+// 
+// Promise
+// Promise bhai asgn hai yaad rakahna , bad me result aai ga , Promise asyc
+
+// randomNum.ceil(randomNum)
+let firstPromise = new Promise(function (resolve, reject) {
     setTimeout(function () {
-        if (email === "za@gmail.com" && password === "090909") {
-            console.log("Ok");
-            callBack(secondCallBack);
+
+        let randomNum = Math.ceil(Math.random() * 100)
+
+        if (randomNum % 2 === 0) {
+            resolve(randomNum)
+        } else {
+            reject("IT is an odd number")
         }
     }, 1000)
-}
+})
 
-signup("za@gmail.com", "090909",
-    function redirectTOTimeLine(cd) {
-        console.log(cd());
-    }, function redi() {
-        return "Wah Bhai Clean Code 😂"
+firstPromise
+    .then(function (success) {
+        console.log("First Promise", success);
+    })
+    .catch(function (error) {
+        console.error(error);
 
-    });
-
+    })
 
